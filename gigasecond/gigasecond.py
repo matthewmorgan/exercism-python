cd402436609d58739f42f6819e72d115fd7818be
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-09 08:40:19 -0500

    .gitignore is now working

--- gigasecond/gigasecond.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-gs_per_s = 1000000000
-
-def add_gigasecond(start):
-    return start + datetime.timedelta(seconds = gs_per_s)
--- pangram/pangram.py
+++
@@ -1,5 +0,0 @@
-alphabet_length = 26
-
-
-def is_pangram(sentence:str) -> bool:
-    return len(set(filter(str.isalnum, sentence))) >= alphabet_length
