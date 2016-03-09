75c3a62e658201d149be6be761b039c2f163d503
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-09 08:33:44 -0500

    Add new exercises

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,14 +1,13 @@
-sort_word = lambda word: ''.join(sorted(word))
-
 def detect_anagrams(word, candidates):
     global sorted_word, original_word
-    original_word = word.lower
-    sorted_word  = sort_word(word).lower
+    original_word = word.lower()
+    sorted_word  = sort_word(word).lower()
     return list(filter(is_anagram, candidates))
 
 def is_anagram(candidate):
-    return sorted_word == sort_word(candidate)  and not_same(candidate)
+    return sorted_word == sort_word(candidate) and not_original_word(candidate)
 
-def not_same(candidate):
-    return original_word != candidate
+def not_original_word(candidate):
+    return original_word != candidate.lower()
 
+sort_word = lambda word: ''.join(sorted(word.lower()))
--- anagram/anagram_test.py
+++ anagram/anagram_test.py
@@ -15,31 +15,31 @@ class AnagramTests(unittest.TestCase):
             ['tan'],
             detect_anagrams('ant', 'tan stand at'.split())
         )
-
+ 
     def test_detect_multiple_anagrams(self):
         self.assertEqual(
             ['stream', 'maters'],
             detect_anagrams('master', 'stream pigeon maters'.split())
         )
- 
+  
     def test_does_not_confuse_different_duplicates(self):
         self.assertEqual(
             [],
             detect_anagrams('galea', ['eagle'])
         )
- 
+  
     def test_eliminate_anagram_subsets(self):
         self.assertEqual(
             [],
             detect_anagrams('good', 'dog goody'.split())
         )
- 
+  
     def test_detect_anagram(self):
         self.assertEqual(
             ['inlets'],
             detect_anagrams('listen', 'enlists google inlets banana'.split())
         )
- 
+  
     def test_multiple_anagrams(self):
         self.assertEqual(
             'gallery regally largely'.split(),
@@ -48,24 +48,24 @@ class AnagramTests(unittest.TestCase):
                 'gallery ballerina regally clergy largely leading'.split()
             )
         )
- 
+  
     def test_anagrams_are_case_insensitive(self):
         self.assertEqual(
             ['Carthorse'],
             detect_anagrams('Orchestra',
                             'cashregister Carthorse radishes'.split())
         )
-# 
-#     def test_same_word_isnt_anagram(self):
-#         self.assertEqual(
-#             [],
-#             detect_anagrams('banana', ['banana'])
-#         )
-# 
-#         self.assertEqual(
-#             [],
-#             detect_anagrams('go', 'go Go GO'.split())
-#         )
+ 
+    def test_same_word_isnt_anagram(self):
+        self.assertEqual(
+            [],
+            detect_anagrams('banana', ['banana'])
+        )
+ 
+        self.assertEqual(
+            [],
+            detect_anagrams('go', 'go Go GO'.split())
+        )
 
 if __name__ == '__main__':
     unittest.main()
