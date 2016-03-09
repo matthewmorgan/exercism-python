8f288810dc00dfe63b76912e046ff0cc1d0f7664
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-09 08:43:21 -0500

    new gitignore

--- hamming/hamming.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-foldl = lambda total, pair: total if pair[0] == pair[1] else total + 1
-
-def distance(strand1, strand2):
-  return reduce(foldl, zip(strand1, strand2), 0)
-
--- rna-transcription/dna.py
+++
@@ -1,6 +0,0 @@
-D = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-f = lambda a: D[a]
-
-def to_rna(dna):
-  rna_map = map(f, dna)
-  return ''.join(rna_map)
