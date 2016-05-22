5920983e7c5ecb93a68a5bc41c7459f3cdb9b9d4
Matthew Morgan <matt@dulcetsoftware.com>
2016-05-22 15:19:43 -0400

    Revise and refactor

--- say/say.py
+++ say/say.py
@@ -14,65 +14,61 @@ def say(num):
     if num < 0 or num > 999999999999:
         raise AttributeError('Number must be between 0 and 999,999,999,999')
 
-    english = parse(num)
+    english = parse_number(num)
     if english.startswith('and '):
         english = english[4:]
     return english
 
 
-def split_by_powers(num):
-    digits = str(num)
+def split_by_powers(digits: str):
     m = math.pow(10, len(digits))
-    converted = []
+    powers = [0, 0, 0]
     while len(digits) > 0:
         digit = digits[0]
         digits = digits[1:len(digits)]
         m = int(m // 10)
-        converted.append(int(digit) * m)
-    return list(reversed(converted))
+        powers[len(digits)] = int(digit) * m
+    return powers
 
 
-def extract_brackets(num):
-    digits = str(num)
-    brackets = []
-    endpoint = len(digits) % 3
-    if endpoint:
-        brackets.append(''.join(digits[0:endpoint]))
-    digits = digits[endpoint: len(digits)]
-    while len(digits) > 0:
-        brackets.append(''.join(digits[0:3]))
-        digits = digits[3: len(digits)]
-    return brackets
+def extract_triplets(num: str):
+    triplets = []
+    remainder_length = len(num) % 3
+    if remainder_length:
+        triplets.append(''.join(num[0:remainder_length]))
+    num = num[remainder_length: len(num)]
+    while num:
+        triplets.append(''.join(num[0:3]))
+        num = num[3: len(num)]
+    return triplets
 
 
-def parse(num):
-    brackets = extract_brackets(num)
+def parse_number(num: int):
     place_name_index = 0
     english = []
-    for digits in reversed(brackets):
-        prepend_and = place_name_index == 0
-        phrase = parse_thousand(digits, prepend_and) + ' '
-        if len(phrase.strip()) > 0:
-            phrase += PLACENAMES[place_name_index]
+    for triplet in reversed(extract_triplets(str(num))):
+        phrase = parse_triplet_to_words(triplet, prepend_and=place_name_index == 0)
+        if phrase:
+            phrase += ' ' + PLACENAMES[place_name_index]
             english.append(phrase.strip())
         place_name_index += 1
     return ' '.join(reversed(english))
 
 
-def parse_thousand(num, prepend_and=False):
-    powers = split_by_powers(int(num))
+def parse_triplet_to_words(num: str, prepend_and: bool):
+    powers = split_by_powers(num)
     segments = ['', '', '']
-    if len(powers) > 1 and powers[1] == 10:
-        index = 10 + powers[0]
-        segments[0] = ONES[index]
+    if powers[1] == 10:
+        teen_index = 10 + powers[0]
+        segments[0] = ONES[teen_index]
     else:
         segments[0] = ONES[powers[0]]
-        if len(powers) > 1:
+        if powers[1]:
             segments[1] = TENS[int(powers[1] / 10)]
     if segments[1] and segments[0]:
         hyphenated = '-'.join(reversed(segments[0:2]))
         segments = ['', hyphenated, '']
-    if len(powers) > 2 and powers[2]:
+    if powers[2]:
         segments[2] = ONES[int(powers[2] / 100)] + ' hundred'
         prepend_and = True
     if prepend_and:
