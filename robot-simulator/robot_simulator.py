003e56140990798ab66b48597f84320b45cb9351
Matt Morgan <matt.morgan@ithaka.org>
2016-06-06 14:49:21 -0400

    Refactor

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -4,7 +4,7 @@ BEARINGS = {NORTH: (0, 1), SOUTH: (0, -1), EAST: (1, 0), WEST: (-1, 0)}
 
 class Robot:
     def __init__(self, bearing=NORTH, x=0, y=0):
-        self.coordinates = (x, y)
+        self.coordinates = x, y
         self.bearing = bearing
         self.possible_moves = {'L': self.turn_left, 'R': self.turn_right, 'A': self.advance}
 
@@ -19,11 +19,10 @@ class Robot:
             self.bearing = NORTH
 
     def advance(self):
-        (dx, dy) = BEARINGS[self.bearing]
-        (x, y) = self.coordinates
-        self.coordinates = (x + dx, y + dy)
+        dx, dy = BEARINGS[self.bearing]
+        x, y = self.coordinates
+        self.coordinates = x + dx, y + dy
 
     def simulate(self, moves):
         for move in moves:
             self.possible_moves[move]()
-            
--- run-length-encoding/run_length.py
+++ run-length-encoding/run_length.py
@@ -1,20 +1,28 @@
 import re
 from functools import reduce
 
+
 def encode(input):
     return ''.join(reduce(compress, tokenize(input), []))
+
 
 def decode(input):
     return reduce(expand, chunk(input), '')
 
 
-tokenize = lambda input: re.finditer(r'(.)\1*', input)
+def tokenize(input):
+    return re.finditer(r'(.)\1*', input)
 
-chunk = lambda input: re.findall(r'(\d*)(\D)', input)
 
-compress = lambda output, match: \
-    output + [('{}{}'.format(len(match.group()), match.group()[0]))] \
-        if len(match.group()) > 1 else output + [match.group()]
+def chunk(input):
+    return re.findall(r'(\d*)(\D)', input)
 
-expand = lambda a, b: \
-    a + b[1] if not b[0] else a + (b[1] * int(b[0]))
+
+def compress(output, match):
+    return output + [('{}{}'.format(len(match.group()), match.group()[0]))] if len(match.group()) > 1 \
+        else output + [match.group()]
+
+
+def expand(a, b):
+    return a + b[1] if not b[0] \
+        else a + (b[1] * int(b[0]))
