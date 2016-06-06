0c76586594ff651f887aacaaeeb71bd96c5a76cf
Matt Morgan <matt.morgan@ithaka.org>
2016-06-06 15:49:04 -0400

    Add prime factors

---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,11 @@
+def prime_factors(num):
+    factors = []
+    current_factor = 2
+
+    while num > 1:
+        if num % current_factor == 0:
+            factors.append(current_factor)
+            num /= current_factor
+        else:
+            current_factor += 1
+    return factors
