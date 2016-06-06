b3ab9222a5433950b99a2bec0feddee0b84f30bb
Matt Morgan <matt.morgan@ithaka.org>
2016-06-06 14:37:44 -0400

    New robot simulator;

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,34 +1,29 @@
-NORTH, EAST, SOUTH, WEST = 1,2,3,4
+NORTH, EAST, SOUTH, WEST = 1, 2, 3, 4
+BEARINGS = {NORTH: (0, 1), SOUTH: (0, -1), EAST: (1, 0), WEST: (-1, 0)}
+
 
 class Robot:
-    def __init__(self, bearing = NORTH, x = 0, y = 0):
-        self.coordinates = (x,y)
+    def __init__(self, bearing=NORTH, x=0, y=0):
+        self.coordinates = (x, y)
         self.bearing = bearing
-        self.possible_moves = {'L':self.turn_left, 'R':self.turn_right, 'A':self.advance}
+        self.possible_moves = {'L': self.turn_left, 'R': self.turn_right, 'A': self.advance}
 
     def turn_left(self):
-        self.bearing = self.bearing - 1
+        self.bearing -= 1
         if self.bearing < NORTH:
             self.bearing = WEST
-            
+
     def turn_right(self):
-        self.bearing = self.bearing + 1
+        self.bearing += 1
         if self.bearing > WEST:
             self.bearing = NORTH
-    
+
     def advance(self):
+        (dx, dy) = BEARINGS[self.bearing]
         (x, y) = self.coordinates
-        if self.bearing == NORTH:
-            y += 1
-        elif self.bearing == SOUTH:
-            y -= 1
-        elif self.bearing == WEST:
-            x -= 1
-        else:
-            x += 1
-        self.coordinates = (x, y)
+        self.coordinates = (x + dx, y + dy)
 
     def simulate(self, moves):
         for move in moves:
             self.possible_moves[move]()
-    
+            
