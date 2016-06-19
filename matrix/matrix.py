b7366c51a4c92065670d9c41de8f616ac2b41e97
Matthew Morgan <matt@dulcetsoftware.com>
2016-06-19 09:51:17 -0400

    Remove unneeded method

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,7 +1,5 @@
 class Matrix:
-    def __init__(self, input):
-        self.parse(input)
-    
-    def parse(self, input):
-        self.rows = [[int(el) for el in row.split(' ')] for row in input.splitlines()]
+    def __init__(self, data):
+        self.rows = [[int(el) for el in row.split(' ')] for row in data.splitlines()]
         self.columns= [list(tup) for tup in zip(*self.rows)]
+        
