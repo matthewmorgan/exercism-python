cfd2f4cacab770ebcce00b432031ff909dd93305
Matthew Morgan <matt@dulcetsoftware.com>
2016-06-22 16:50:13 -0400

    No use of int()

--- binary/binary.py
+++ binary/binary.py
@@ -4,4 +4,4 @@ from re import fullmatch
 def parse_binary(binary='0'):
     if not fullmatch(r'[01]+', binary):
         raise ValueError()
-    return sum([int(b) * 2 ** p for p, b in enumerate(reversed(list(binary)))])
+    return sum([2 ** p if b == '1' else 0 for p, b in enumerate(reversed(list(binary)))])
