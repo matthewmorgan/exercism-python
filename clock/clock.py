5dd96e445b71ad37983f460d2c943f01dc8eef6b
Matthew Morgan <matt@dulcetsoftware.com>
2016-10-14 10:47:33 -0400

    Pass all tests

--- clock/clock.py
+++ clock/clock.py
@@ -1,5 +1,22 @@
-def Clock(h, m):
-    minutes = m + h * 60
-    hours = round(minutes/60,0) % 24
-    minutes = minutes % 60
-    return "%02d:%02d" % (hours, minutes)
+import math
+
+
+class Clock():
+    def __init__(self, h, m):
+        self.normalize(h,m)
+
+    def normalize(self, h, m):
+        self.minutes = m + h * 60
+        self.hours = math.floor(self.minutes/60) % 24
+        self.minutes %= 60
+
+    def __str__(self):
+        return "%02d:%02d" % (self.hours, self.minutes)
+
+    def add(self, delta_minutes):
+        self.minutes += delta_minutes
+        self.normalize(self.hours, self.minutes)
+        return self
+
+    def __eq__(self, other):
+        return self.__str__() == other.__str__()
