71159418bc2338197a7a6645ac3aa42a4d8f33d5
Matthew Morgan <matt@dulcetsoftware.com>
2016-12-18 12:59:50 -0500

    Refactor

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,6 +1,5 @@
 def flatten(arr):
-    arr = arr or []
     result = []
-    for el in arr:
+    for el in arr or []:
         result.append(el) if isinstance(el, int) or isinstance(el, str) else result.extend(flatten(el))
     return result
