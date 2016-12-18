2c93b66e54be4217f72645589617a490da25fb92
Matthew Morgan <matt@dulcetsoftware.com>
2016-12-18 13:10:47 -0500

    Cleaner type checking

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,5 +1,5 @@
 def flatten(arr):
     result = []
     for el in arr or []:
-        result.append(el) if isinstance(el, int) or isinstance(el, str) else result.extend(flatten(el))
+        result.append(el) if type(el) in [int, str] else result.extend(flatten(el))
     return result
