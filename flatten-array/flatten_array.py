2b2f558421d9a8c9663e52ea471e0b5c0d4bf84c
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-03 13:14:51 -0400

    minor refactor

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,5 +1,7 @@
 def flatten(arr):
+    if arr is None:
+        return []
     result = []
-    for el in arr or []:
+    for el in arr:
         result.append(el) if type(el) in [int, str] else result.extend(flatten(el))
     return result
