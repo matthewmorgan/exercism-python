92749b4dc910175888dc450f8b7577d1b5a8c79a
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-04 14:28:26 -0400

    Clean up solution

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,10 +1,16 @@
-def rotate(value, offset):
-    return ''.join([map(letter, offset) for letter in value])
+ALPHA_LENGTH = 26
+ALPHA_START = 97
 
-def map(value, offset):
-    if not value.isalpha():
-        return value
-    if value.lower() != value:
-        value = value.lower()
-        return chr((ord(value) - 97 + offset) % 26 + 97).upper()
-    return chr((ord(value) - 97 + offset) % 26 + 97)
+
+def rotate(value, offset):
+    return ''.join([map_letter(letter, offset) for letter in value])
+
+
+def map_letter(letter, offset):
+    if not letter.isalpha():
+        return letter
+    return encode(letter.lower(), offset).upper() if letter.isupper() else encode(letter, offset)
+
+
+def encode(letter, offset):
+    return chr((ord(letter) - ALPHA_START + offset) % ALPHA_LENGTH + ALPHA_START)
