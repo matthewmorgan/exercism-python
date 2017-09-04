74b0ed665b5e319913b9b1fb6c1ffbe47ca566d0
Matthew Morgan <matt@dulcetsoftware.com>
2017-09-04 08:06:33 -0400

    solution without filter

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(phrase):
-    lowercase_letters_only = list(filter(str.isalpha, phrase.lower()))
+    lowercase_letters_only = [letter for letter in phrase.lower() if letter.isalpha()]
     return len(set(lowercase_letters_only)) == len(lowercase_letters_only)
